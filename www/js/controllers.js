7a97b92388f08b31bf58f34ccbf8a46d261b6dca
Ervin Mamutov <erviin@live.ie>
2016-04-21 14:30:50 +0100

    Color changed back and default tasks deleted

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,69 +2,75 @@ angular.module('starter.controllers', [])
 
 .controller('TaskCtrl', function($scope, Tasks) {
 
-  $scope.data = {
-      taskName: "",
-      taskDetail: ""
-  };
+    $scope.data = {
+        taskName: "",
+        taskDetail: ""
+    };
+
+    $scope.tasks = Tasks.all();
     
-  $scope.tasks = Tasks.all();
-  $scope.remove = function(task) {
-    Tasks.remove(task);
-  };
-    
-  $scope.addTask = function() {
-      if($scope.data.taskName === "" && $scope.data.taskDetail === ""){
-          console.log("empty");
-      }else if ($scope.data.taskName == ""){
-          alert("Please specify a task name");
-      }else{
-          if($scope.data.taskDetail == ""){
-              $scope.data.taskDetail = "No Details";
-              Tasks.addTask($scope.data.taskName, $scope.data.taskDetail);
-              $scope.data.taskName = "";
-   $scope.data.taskDetail = "";
-          }else{
-      Tasks.addTask($scope.data.taskName, $scope.data.taskDetail);
-      
-      $scope.data.taskName = "";
-   $scope.data.taskDetail = "";
-          }
-  } 
-  }
-  
+    $scope.remove = function(task) {
+        Tasks.remove(task);
+    };
+
+    $scope.addTask = function() {
+        if($scope.data.taskName === "" && $scope.data.taskDetail === ""){
+            console.log("empty");
+        }else if ($scope.data.taskName == ""){
+            alert("Please specify a task name");
+        }else{
+            if($scope.data.taskDetail == ""){
+                $scope.data.taskDetail = "No Details";
+                
+                Tasks.addTask($scope.data.taskName, $scope.data.taskDetail);
+                
+                $scope.data.taskName = "";
+                $scope.data.taskDetail = "";
+            }else{
+                Tasks.addTask($scope.data.taskName, $scope.data.taskDetail);
+
+                $scope.data.taskName = "";
+                $scope.data.taskDetail = "";
+            }
+        }
+    }
+
 })
 
 .controller('TaskDetailCtrl', function($scope, $stateParams, Tasks) {
-  $scope.data = {
-      taskName: "",
-      taskDetail: ""
-  };  
-      
-     $scope.editTask = function() {
-        
-         if($scope.data.taskName === "" && $scope.data.taskDetail === ""){
-             
-         }
-         else if($scope.data.taskName === ""){
-             
+    $scope.data = {
+        taskName: "",
+        taskDetail: ""
+    };  
+
+    $scope.editTask = function() {
+
+        if($scope.data.taskName === "" && $scope.data.taskDetail === ""){
+
+        }
+        else if($scope.data.taskName === ""){
             $scope.data.taskName = Tasks.getTask($stateParams.taskId).name;
-             Tasks.editTask($stateParams.taskId, $scope.data.taskName, $scope.data.taskDetail)
-      $scope.data.taskName = "";
-   $scope.data.taskDetail = "";
-         }else if($scope.data.taskDetail === ""){
-             
-             $scope.data.taskDetail = Tasks.getTask($stateParams.taskId).taskDetail;
-             Tasks.editTask($stateParams.taskId, $scope.data.taskName, $scope.data.taskDetail)
-      $scope.data.taskName = "";
-   $scope.data.taskDetail = "";
-         }else{
-             Tasks.editTask($stateParams.taskId, $scope.data.taskName, $scope.data.taskDetail)
-      $scope.data.taskName = "";
-   $scope.data.taskDetail = "";
-         }
-         
-  }  
-      
-  $scope.task = Tasks.getTask($stateParams.taskId);
+            
+            Tasks.editTask($stateParams.taskId, $scope.data.taskName, $scope.data.taskDetail)
+            
+            $scope.data.taskName = "";
+            $scope.data.taskDetail = "";
+        }else if($scope.data.taskDetail === ""){
+            $scope.data.taskDetail = Tasks.getTask($stateParams.taskId).taskDetail;
+            
+            Tasks.editTask($stateParams.taskId, $scope.data.taskName, $scope.data.taskDetail)
+            
+            $scope.data.taskName = "";
+            $scope.data.taskDetail = "";
+        }else{
+            Tasks.editTask($stateParams.taskId, $scope.data.taskName, $scope.data.taskDetail)
+            
+            $scope.data.taskName = "";
+            $scope.data.taskDetail = "";
+        }
+
+    }  
+
+    $scope.task = Tasks.getTask($stateParams.taskId);
 })
 ;
--- www/js/services.js
+++ www/js/services.js
@@ -1,51 +1,47 @@
 angular.module('starter.services', [])
 
 .factory('Tasks', function() {
-  var tasks = {
-  data:
-  [{
-    id: 0,
-    name: 'Buy Milk',
-    taskDetail: 'Stop by the shop',
-    added: new Date()
-  }, {
-    id: 1,
-    name: 'Wash Car',
-    taskDetail: 'Dont forget to hoover',
-    added: new Date()  
-  }]};
-    
-  function addTask(taskName, taskDetail) {
-      tasks.data.push({id: tasks.data.length+1, name: taskName, taskDetail: taskDetail, added: new Date()});
-  }    
-    
-  function editTask(taskId, taskName, taskDetail){
-      
-      for (var i = 0; i < tasks.data.length; i++) {
-        if (tasks.data[i].id === parseInt(taskId)) {
-            tasks.data[i].name = taskName;
-            tasks.data[i].taskDetail = taskDetail;
+    var tasks = {
+        data:
+        [{
+            id: 0,
+            name: 'Sample Task',
+            taskDetail: 'Sample Task',
+            added: new Date()
+        }]       
+    };
+
+    function addTask(taskName, taskDetail) {
+        tasks.data.push({id: tasks.data.length+1, name: taskName, taskDetail: taskDetail, added: new Date()});
+    }    
+
+    function editTask(taskId, taskName, taskDetail){
+        for (var i = 0; i < tasks.data.length; i++) {
+            if (tasks.data[i].id === parseInt(taskId)) {
+                tasks.data[i].name = taskName;
+                tasks.data[i].taskDetail = taskDetail;
+            }
         }
-      }
-      
-  }
-    
-  return {
-    all: function() {
-      return tasks.data;
-    },
-    remove: function(task) {
-      tasks.data.splice(tasks.data.indexOf(task), 1);
-    },
-    addTask: addTask,
-    editTask: editTask,  
-    getTask: function(taskId) {
-      for (var i = 0; i < tasks.data.length; i++) {
-        if (tasks.data[i].id === parseInt(taskId)) {
-          return tasks.data[i];
-        }
-      }
-      return null;
+
     }
-  };
-});
+
+    return {
+        all: function() {
+            return tasks.data;
+        },
+        remove: function(task) {
+            tasks.data.splice(tasks.data.indexOf(task), 1);
+        },
+        addTask: addTask,
+        editTask: editTask,  
+        getTask: function(taskId) {
+            for (var i = 0; i < tasks.data.length; i++) {
+                if (tasks.data[i].id === parseInt(taskId)) {
+                return tasks.data[i];
+                }
+            }
+            return null;
+        }
+    };
+})
+;
