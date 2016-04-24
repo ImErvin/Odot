11d8198b517dc748518e8ba426bcb89db75b8543
Ervin Mamutov <erviin@live.ie>
2016-04-24 14:47:00 +0100

    Comments finished and apk added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,18 +4,21 @@ angular.module('starter.controllers', [])
 TaskCtrl parses in a $scope and the factory Tasks. $scope is used to manipulate and call variables and functions from the controllers in the HTML files and the controllers will manipulate and call variables and functions from the Tasks factory.  */
 .controller('TaskCtrl', function($scope, Tasks) {
     
-    /*  */
+    /* Controlled by ng-model in the HTML */
     $scope.data = {
         taskName: "",
         taskDetail: ""
     };
-
+    
+    /* Calls Factory to retrieve the object array */
     $scope.tasks = Tasks.all();
     
+    /* Makes a function remove that is controlled by ng-click and it parses in the current Item into the factory function */
     $scope.remove = function(task) {
         Tasks.remove(task);
     };
 
+    /* Makes a function to add a task, it's controlled by ng-click and parses in the above data object details into the factory. If/else statements validate the input i.e: If they don't enter a task detail, it will automatically show "No Details" etc.  */
     $scope.addTask = function() {
         if($scope.data.taskName === "" && $scope.data.taskDetail === ""){
             console.log("empty");
@@ -41,16 +44,19 @@ TaskCtrl parses in a $scope and the factory Tasks. $scope is used to manipulate 
     
 })
 
+/* The TaskDetailCtrl parses in a $scope, $stateParams to work between each task state and the factory Tasks */
 .controller('TaskDetailCtrl', function($scope, $stateParams, Tasks) {
+    /* Object for new details for the edit function */
     $scope.data = {
         taskName: "",
         taskDetail: ""
     };  
-
+    
+    /* Edit function will rename the task name or task details in the object. It parses in the new information to the factory. The If/Else is validation i.e; The user only wants to change the name but not the details. $stateParams allows the edit function to keep the current id of the task as we don't want to edit that. */
     $scope.editTask = function() {
 
         if($scope.data.taskName === "" && $scope.data.taskDetail === ""){
-
+            
         }
         else if($scope.data.taskName === ""){
             $scope.data.taskName = Tasks.getTask($stateParams.taskId).name;
@@ -74,7 +80,8 @@ TaskCtrl parses in a $scope and the factory Tasks. $scope is used to manipulate 
         }
 
     }  
-
+    
+    /* Calls the getTask function and parses in the stateParams for */
     $scope.task = Tasks.getTask($stateParams.taskId);
 })
 ;
--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,9 @@
  angular.module('starter.services', [])
 
+ /* Single factory called Tasks, Tasks is used in both controllers */
 .factory('Tasks',['$http',function($http){
+    
+    /* Object Array holding the task id, name, details and the added date. This is the main variable of the app, it gets manipulated by all functions. */
    var tasks = {
         data:
         [{
@@ -11,10 +14,13 @@
         }]       
     };
     
+    /* The addTask parses in a taskName and taskDetail from the controller and creates a new object in the array using the .push method.
+    id: is set by the length of the object array plus 1, so they will never be identical. The name is parsed in from the controller which is set by a form in the html, the detail is also parsed in from the controller. The added date uses the function Date(). Code is sampled from solution-factories and modified by me. */
     function addTask(taskName, taskDetail) {
         tasks.data.push({id: tasks.data.length+1, name: taskName, taskDetail: taskDetail, added: new Date()});
     }    
-
+    
+    /* EditTask function is used to edit the name and details of a task. It parses in 3 variables, an Id, taskName and taskDetails. Similar to the getTask function, it loops through the object array and when the parsed id matches the object id, it sets a new name and details for that current task. */
     function editTask(taskId, taskName, taskDetail){
         for (var i = 0; i < tasks.data.length; i++) {
             if (tasks.data[i].id === parseInt(taskId)) {
@@ -22,9 +28,9 @@
                 tasks.data[i].taskDetail = taskDetail;
             }
         }
-
     }
-
+    
+    /* Each factory must have a return statement, here we return all which returns the object array. remove which uses splice to remove an object from the array. It parses in a task which is set by the ion-list/ion-item. The addTask allows the controller to use the addTask function, editTask allows the controller to use the editTask function and getTask is used to display the view of a specific task, as every task has it's own state. */
     return {
         all: function() {
             return tasks.data;
@@ -41,15 +47,7 @@
                 }
             }
             return null;
-        },set:function(key,value){
-          return localStorage.setItem(key,JSON.stringify(value));
-       },
-       get:function(key){
-         return JSON.parse(localStorage.getItem(key));
-       },
-       destroy:function(key){
-         return localStorage.removeItem(key);
-       },
+        }
     };
 }])
 ;
